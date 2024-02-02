711b2d9eb1831d5098d940643fcdf2a539249d3b
Mohit Gupta <mohitproduct@gmail.com>
2024-02-02 07:13:24 +0530

    getKthNode

--- linkedList.py
+++ linkedList.py
@@ -1,19 +1,42 @@
 
 class Node:
-    def __init__(self, data, next) -> None:
-        self.data = data
+    def __init__(self, val) -> None:
+        self.val = val
         self.next = None
 
 class linkedList:
     def __init__(self, root) -> None:
-        self.head = root
-        self.next = None
-        self.tail = None
+        self.head = Node(root)
     
     def add(self,data):
-        ptr = self.next
-        while ptr:
+        ptr = self.head
+        while ptr.next:
             ptr = ptr.next
-        ptr = Node(data)
+        ptr.next = Node(data)
 
+    def getLength(self):
+        length = 1
+        ptr = self.head
+        while ptr.next:
+            length += 1
+            ptr = ptr.next
+        return length
+    
+    def getKthNode(self,k):
+        if k > self.getLength():
+            raise IndexError("k is greater than length of lindeList")
+        
+        ptr = self.head
+        for i in range(k-1):
+            ptr = ptr.next
+        return ptr
+
+lis = linkedList("one")
+lis.add("two")
+lis.add("three")
+print(lis.getLength())
+kthNode = lis.getKthNode(2)
+print(kthNode.val)
+anotherKthNode = lis.getKthNode(4)
+print(anotherKthNode.val)
 
