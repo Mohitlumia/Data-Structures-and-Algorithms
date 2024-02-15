f77e564c81c70f5b8f02e8edbb08410b7cea47ad
Mohit Gupta <mohitproduct@gmail.com>
2024-02-15 17:40:43 +0530

    DFS_BinaryTree initial commit

---
+++ Algorithms/Search/DepthFirstSearch/DFS_BinaryTree.py
@@ -0,0 +1,12 @@
+
+# dfs in binary tree is identical to inorder traversal
+
+def DFS_BinaryTree(root):
+    if not root:
+        return
+    #left
+    DFS_BinaryTree(root.left)
+    # mid
+    print(root.val)
+    # right
+    DFS_BinaryTree(root.right)
