014231ed56ff409d80551f7481c9efaf1378f7a6
Mohit Gupta <mohitproduct@gmail.com>
2024-02-17 08:05:27 +0530

    Graph initial commit

---
+++ DataStructures/Graph/Graph.py
@@ -0,0 +1,36 @@
+
+
+class Graph:
+
+    def __init__(self, edges):
+        self.edges = edges
+        self.graph_dict = {}
+        for start, end in edges:
+            if start in self.graph_dict:
+                self.graph_dict[start].append(end)
+            else:
+                self.graph_dict[start] = [end]
+            
+        print("Graph Dict:", self.graph_dict)
+
+
+
+if __name__ == '__main__':
+
+    # routes are the edges which connects two nodes
+    # let say Mumbai and Pune called as node pair
+    routes = [
+        ("Mumbai","Pune"),
+        ("Mumbai", "Surat"),
+        ("Surat", "Bangaluru"),
+        ("Pune","Hyderabad"),
+        ("Pune","Mysuru"),
+        ("Hyderabad","Bangaluru"),
+        ("Hyderabad", "Chennai"),
+        ("Mysuru", "Bangaluru"),
+        ("Chennai", "Bangaluru")
+    ]
+
+
+    route_graph = Graph(routes)
+
