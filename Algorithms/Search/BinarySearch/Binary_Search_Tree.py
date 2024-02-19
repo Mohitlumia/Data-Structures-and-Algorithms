0a48f18e858244364cb2525368022e37a197e541
mohit.gupta.mae <mohitproduct@gmail.com>
2024-02-19 17:44:53 +0530

    add inorder

--- Algorithms/Search/BinarySearch/Binary_Search_Tree.py
+++ Algorithms/Search/BinarySearch/Binary_Search_Tree.py
@@ -4,29 +4,36 @@ class binarySearchTree:
         self.val = data
         self.left = left
         self.right = right
-    
+        
     def add_node(self,data):
         if self.val == data:
             return
         if data < self.val:
             if self.left:
-                return self.left.add_node(data)
+                self.left.add_node(data)
             else:
                 self.left = binarySearchTree(data)
         else:
             if self.right:
-                return self.right.add_node(data)
+                self.right.add_node(data)
             else:
                 self.right = binarySearchTree(data)
-
-               
                 
-def get_bst(array):
-    root = binarySearchTree(array[0])
-    for element in array[1:]:
-        root.add_node(element)
-    return
+    def inorder(self):
+        order = []
+        if self.left:
+            order += self.left.inorder()
+        order.append(self.val)
+        if self.right:
+            order += self.right.inorder()
+        return order
+
 
 nums = [9,12,3,4,6,5,7,78,34,34]
 
-bst_root = get_bst(nums)
+root = binarySearchTree(nums[0])
+
+for element in nums:
+   root.add_node(element)
+
+print(root.inorder())
