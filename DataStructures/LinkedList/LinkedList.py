9dab3ee40707824e4f628525bbb9bc6c4b0ad2ff
Mohit Gupta <mohitproduct@gmail.com>
2024-02-22 16:52:41 +0530

    remove at index

--- DataStructures/LinkedList/LinkedList.py
+++ DataStructures/LinkedList/LinkedList.py
@@ -21,10 +21,26 @@ class linkedList:
             return [self.val] + self.next.getList()
         else:
             return [self.val]
+    
+    def remove(self,index):
+        if index in range(self.length()):
+            if index == 0:
+                return self.next
+            else:
+                self.next = self.next.remove(index-1)
+                return self
+        else:
+            raise IndexError("Index out of range")
 
 
 
 lis = linkedList(0)
-lis.add(1)
+
+for i in range(1,10):
+    lis.add(i)
+
+print(lis.getList()) # get linkedList as a list
+print(lis.length()) # get length of linkedList
+lis = lis.remove(10) # remove likedList node at index
 print(lis.getList())
 print(lis.length())
