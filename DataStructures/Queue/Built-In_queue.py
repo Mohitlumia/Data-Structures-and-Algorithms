c717ff23c15ee5417acd98d0826bf6734254a916
mohit.gupta.mae <mohitproduct@gmail.com>
2024-02-22 10:10:26 +0530

    add peek method

--- DataStructures/Queue/Built-In_queue.py
+++ DataStructures/Queue/Built-In_queue.py
@@ -1,7 +1,18 @@
 from queue import Queue
 
+# Inherit Queue and add peek method
+class MyQueue(Queue):
+    def peek(self):
+        """Return the item at the front of the queue without removing it."""
+        if not self.empty():
+            return self.queue[0]
+        else:
+            print("Queue is empty")
+            return None
+          
+
 # Create a queue
-q = Queue()
+q = MyQueue()
 
 # Enqueue elements
 q.put(1)
@@ -11,3 +22,6 @@ print(q.get())  # Output: 1
 
 # Check if the queue is empty
 print(q.empty())  # Output: True
+
+# peek element
+print(q.peek())
