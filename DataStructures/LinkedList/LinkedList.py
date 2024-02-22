d32e92c0566a72c6961470897f55d502abbc47bf
Mohit Gupta <mohitproduct@gmail.com>
2024-02-22 17:20:29 +0530

    add at gien index

--- DataStructures/LinkedList/LinkedList.py
+++ DataStructures/LinkedList/LinkedList.py
@@ -31,16 +31,31 @@ class linkedList:
                 return self
         else:
             raise IndexError("Index out of range")
+    
+    def addIndex(self,data,index=0):
+        if index in range(self.length()):
+            if index == 0:
+                new = linkedList(data,self)
+                return new
+            else:
+                self.next = self.next.addIndex(data,index-1)
+                return self
+        else:
+            raise IndexError("Index out of range")
 
 
 
+# Create linkedList object
 lis = linkedList(0)
 
+# add nodes to the object
 for i in range(1,10):
     lis.add(i)
 
-print(lis.getList()) # get linkedList as a list
-print(lis.length()) # get length of linkedList
-lis.remove(1) # remove likedList node at index
+
+print(lis.getList())    # get linkedList as a list
+print(lis.length())     # get length of linkedList
+lis.remove(1)           # remove likedList node of the given index
+lis.addIndex(100,1)     # add value at the given index
 print(lis.getList())
 print(lis.length())
