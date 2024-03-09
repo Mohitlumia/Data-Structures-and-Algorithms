e0fcd277dffb7fc501730bee2fdf1b154f084e30
Mohit Gupta <mohitproduct@gmail.com>
2024-03-09 17:52:32 +0530

    bfs in graph

---
+++ Algorithms/Search/BreathFirstSearch/BFS_Graph.py
@@ -0,0 +1,44 @@
+from collections import defaultdict
+
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def add_edge(self, u, v):
+        self.graph[u].append(v)
+
+    def bfs(self, start):
+        visited = set()
+        queue = [start]
+        traversal = []
+
+        while queue:
+            vertex = queue.pop(0)
+            if vertex not in visited:
+                traversal.append(vertex)
+                visited.add(vertex)
+                queue.extend([neighbor for neighbor in self.graph[vertex] if neighbor not in visited])
+
+        return traversal
+
+# Graph:
+#
+#     0 -----> 1
+#     ^ \     / |
+#     |   \  /  |
+#     |    \/   |
+#     |    /\   v
+#     |   /  \> 2 -----> 3
+#     |  /         ^
+#      \<----------|
+
+# Example usage:
+g = Graph()
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(1, 2)
+g.add_edge(2, 0)
+g.add_edge(2, 3)
+g.add_edge(3, 3)
+
+print("BFS Traversal:", g.bfs(2))
