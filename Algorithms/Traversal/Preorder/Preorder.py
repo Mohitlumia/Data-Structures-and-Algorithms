401068866d2e36230054da88fd203d1542e05cc2
Mohit Gupta <mohitproduct@gmail.com>
2024-03-09 17:54:59 +0530

    preorder

--- Algorithms/Traversal/Preorder/Preorder.py
+++ Algorithms/Traversal/Preorder/Preorder.py
@@ -2,13 +2,40 @@
 # preorder traversal takes root of a binary tree as an argument
 # and print node values in preorder
 
-def preorder(root):
-    if not root:
-        return
-    # mid
-    print(root.val)
-    #left
-    preorder(root.left)
-    # right
-    preorder(root.right)
+class TreeNode:
+    def __init__(self, value):
+        self.val = value
+        self.left = None
+        self.right = None
+
+def preorder_traversal(root):
+    if root is None:
+        return []
+    stack = [root]
+    result = []
+    while stack:
+        current = stack.pop()
+        result.append(current.val)
+        if current.right is not None:
+            stack.append(current.right)
+        if current.left is not None:
+            stack.append(current.left)
+    return result
+
+# Example usage:
+#     1
+#    / \
+#   2   3
+#  / \
+# 4   5
+# Preorder traversal: [1, 2, 4, 5, 3]
+
+# Constructing the tree
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+
+print("Preorder Traversal:", preorder_traversal(root))
 
